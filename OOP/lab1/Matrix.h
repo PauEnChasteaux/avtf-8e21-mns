23e722f03931c61d637e7282c7562db429596451
PauEnChasteaux <149692118+PauEnChasteaux@users.noreply.github.com>
2023-11-06 17:39:30 +0700

    Update Matrix.h

--- OOP/lab1/Matrix.h
+++ OOP/lab1/Matrix.h
@@ -6,26 +6,22 @@ class Matrix {
     int rows;
     int colms;
     std::vector<std::vector<double>> m;
-    //std::vector<std::vector<double>> getCofactor(const std::vector<std::vector<double>> vect, int size);
-    //void getCofactor(std::vector<std::vector<double>> vect, std::vector<std::vector<double>> temp, int p, int q, int n);
-    //void adjoint(std::vector<std::vector<double>> vect, std::vector<std::vector<double>> temp);
+    std::vector<std::vector<double>> rtnM();
 public:
-    Matrix() : rows(0), colms(0), m{ 0 }
-    {}
-    Matrix(int rows, int colms) : rows(rows), colms(colms), m{}
-    {}
-    Matrix(int rows, int colms, std::vector<std::vector<double>> m) : rows(rows), colms(colms), m{}
-    {}
+    Matrix();
+    Matrix(int rows, int colms);
     Matrix(std::string str);
 
-    int rtnRows() { return rows; }
-    int rtnColms() { return colms; }
-    std::vector<std::vector<double>> rtnM() { return m; }
+    int rtnRows();
+    int rtnColms();
+    
+    Matrix(const Matrix& m1);
+    Matrix& operator=(const Matrix& m1);
 
     void printMatrix();
-    void miltOnNum(double num);
+    Matrix miltOnNum(double num);
     Matrix transp();
-    double det(/*std::vector<std::vector<double>> m, int rows*/);
+    double det();
 
     Matrix inverse();
 
