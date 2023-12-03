39fd7bc8420908341bd8bb257430dea67d807dad
PauEnChasteaux <149692118+PauEnChasteaux@users.noreply.github.com>
2023-12-04 00:31:57 +0700

    Update main.cxx

--- OOP/lab2/main.cxx
+++ OOP/lab2/main.cxx
@@ -42,56 +42,62 @@ int main(int argc, char** argv)
         case 1:
         {
             int c1 = 0;
-            std::cout << "Если хотите ввести строку вручную введите 1 \n" <<"Если хотите подгрузить строку из файла введите 2 \n" ;
+            std::cout << "Если хотите ввести строку вручную введите 1 \n" << "Если хотите подгрузить строку из файла введите 2 \n";
             std::cin >> c1;
             if (c1 == 1) {
                 std::cout << "Введите строку: \n";
                 std::string str;std::getline(std::cin, str);
-                Lines tempLine(str,1);
+                Lines tempLine(str, 1);
                 s1 = tempLine;
                 break;
             }
             if (c1 == 2) {
                 std::cout << "Введите название файла: ";
-                std::string fStr="text.txt";
+                std::string fStr = "text.txt";
                 //std::getline(std::cin, fStr);
-                std::cout<<'\n'<<fStr<<'\n';
-                Lines tempLine(fStr,2);
+                std::cout << '\n' << fStr << '\n';
+                Lines tempLine(fStr, 2);
                 s1 = tempLine;
                 break;
             }
-            std::cout<< "Попробуйте ещё раз. \n";
+            std::cout << "Попробуйте ещё раз. \n";
         };
         break;
         case 2:
         {
-			std::cout<<"Введите искомую подстроку: \n";
-			std::string str;std::getline(std::cin, str);
+            std::cout << "Введите искомую подстроку: \n";
+            std::string str;std::getline(std::cin, str);
             std::cout << s1.substring(str) << '\n';
         };
         break;
         case 3:
         {
-			std::cout<<"Если хотите выгрузить расспределение в txt-файл нажмите 1 \n" 
-			         <<"Если хотите выгрузить расспределение в csv-файл нажмите 2 \n" ;
-			std::cout << "Введите название файла выгрузки: \n";
+            std::cout<<"Порядок слов в отображаемом результате может быть: \n"
+                     <<"1. Алфавитный.\n"
+                     <<"2. По порядку слов в тексте.\n"
+                     <<"3. По количеству вхождений.\n"
+                     <<"Выберите желаемый порядок: ";
+            int porydok;std::cin >> porydok;
+            std::cout << "Если хотите выгрузить расспределение в txt-файл нажмите 1 \n"
+                << "Если хотите выгрузить расспределение в csv-файл нажмите 2 \n";
+            std::cout << "Введите название файла выгрузки: \n";
             std::string str;std::getline(std::cin, str);
-			int c1 = 0;
-			std::cin >> c1;
+            int c1 = 0;
+            std::cin >> c1;
             if (c1 == 1) {
-                s1.outInTextFile(str);
+                s1.outInTextFile(str,porydok);
                 break;
             }
             if (c1 == 2) {
-                s1.outInCSVFile(str);
+                s1.outInCSVFile(str, porydok);
                 break;
             }
-            std::cout<< "Попробуйте ещё раз. \n";
-		};
-		break;
-		case 4:
+            std::cout << "Попробуйте ещё раз. \n";
+        };
+        break;
+        case 4:
         {
-			std::cout << s1.wrldCount() << '\n';
+            std::cout << s1.wrldCount() << '\n';
         };
         default:
             continue;
@@ -102,5 +108,5 @@ int main(int argc, char** argv)
 
 
 
-	return 0;
+    return 0;
 }
