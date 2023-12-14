1d801d437c7697e55703ba8a54164b7c0dfbab17
PauEnChasteaux <149692118+PauEnChasteaux@users.noreply.github.com>
2023-12-14 22:58:01 +0700

    Update main.cxx

--- OOP/lab3/main.cxx
+++ OOP/lab3/main.cxx
@@ -4,15 +4,17 @@
 
 int main(int argc, char** argv){
     int ch;
-    List list();
+    List list;
     do {
         std::cin>>ch;
         switch (ch) {
-            case 1: list.push();;
+            case 1: list.print();;
             break;
             // case 2: list.deleteNode(&head,posFind(head));
             // break;
-            case 3: list.printList();
+            case 3:
+				int data;std::cin>>data;
+				list.push(data);
             break;
             case 0: std::cout<<"Exit\n";
             break;
