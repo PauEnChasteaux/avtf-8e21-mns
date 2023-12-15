e39cf5053e85ffbf669e8f06e4c56dacd77e947d
PauEnChasteaux <149692118+PauEnChasteaux@users.noreply.github.com>
2023-12-15 09:55:31 +0700

    Update container.hpp

--- OOP/lab3/container.hpp
+++ OOP/lab3/container.hpp
@@ -5,30 +5,36 @@
 #include <string>
 #include <vector>
 
-class Container{
+class Container {
+	
 public:
+	int size;
+
 	Container();
+
+	int getSize();
+
 	~Container();
 };
 
-class List:public Container {
-    struct Node{
+class List :public Container {
+	struct Node {
 		Node* pNext;
 		int data;
-    };
-    Node* head = nullptr;
-    int size;
-	public:
-
-		List();
-        //List(int data);
-		List(int data, Node* pNext = nullptr);
-
-		// Node& operator=(const Node& node);
-
-		void push(int num);
-		void print();
 	};
+	Node* head = nullptr;
+public:
+
+	List();
+	//List(int data);
+	List(int data, Node* pNext = nullptr);
+
+	// Node& operator=(const Node& node);
+
+	void push(int num);
+	void del(int pos);
+	void print();
+};	
 
 
 #endif /* CONTAINER_HPP */
