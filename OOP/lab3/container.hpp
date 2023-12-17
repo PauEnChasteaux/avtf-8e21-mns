2f117ef5bb0c3edef7f19f739c18e5a615642f56
PauEnChasteaux <149692118+PauEnChasteaux@users.noreply.github.com>
2023-12-18 00:38:33 +0700

    Update container.hpp

--- OOP/lab3/container.hpp
+++ OOP/lab3/container.hpp
@@ -1,66 +1,13 @@
 #pragma once
-#ifndef CONTAINER_HPP
-#define CONTAINER_HPP
 
-#include <string>
-#include <vector>
+#include <iostream>
 
 class Container {
-
 public:
-	int size;
-	
-	Container();
-
-	int getSize();
-
-	~Container();
+	virtual void push(int num) = 0;
+	virtual void insert(int index, int data) = 0;
+	virtual void del(int pos) = 0;
+	virtual void printAll() = 0;
+	virtual int getSize() = 0;
+	virtual int getData(int pos) = 0;
 };
-
-class List :public Container {
-	struct Node {
-		Node* pNext;
-		int data;
-	};
-	Node* head = nullptr;
-public:
-
-	List();
-	List(int data, Node* pNext = nullptr);
-
-	List(const List& m1);
-	List& operator=(const List& m1);
-
-	void push(int num);
-	void del(int pos);
-
-	int getData(int pos);
-	void printAll();
-
-	~List();
-};
-
-class dList :public Container {
-	struct Node {
-		Node* pNext;
-		Node* pPrev;
-		int data;
-	};
-	Node* head = nullptr;
-
-public:
-	dList();
-	dList(int data, Node* pNext = nullptr);
-
-	dList(const dList& m1);
-	dList& operator=(const dList& m1);
-
-	void push(int num);
-	void del(int pos);
-
-	int getData(int pos);
-	void printAll();
-
-	~dList();
-};
-#endif /* CONTAINER_HPP */
