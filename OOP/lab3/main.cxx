66fc93ea8fdbf7878e83e35656528575e619aed7
PauEnChasteaux <149692118+PauEnChasteaux@users.noreply.github.com>
2023-12-17 21:38:04 +0700

    Update main.cxx

--- OOP/lab3/main.cxx
+++ OOP/lab3/main.cxx
@@ -9,19 +9,33 @@ int main(int argc, char** argv) {
         std::cin >> ch;
         switch (ch) {
         case 1:
+            std::cout << "Elemet to push: ";
             int data; std::cin >> data;
             list.push(data);
+            std::cout << '\n' << "Done" << '\n';
             break;
-        case 2: 
-            int pos; std::cin >> pos;
-            list.del(pos);
+        case 2:
+            std::cout << list.getSize() << '\n';
             break;
         case 3:
-            list.print();;
+            std::cout << "Elemet to del: ";
+            int pos; std::cin >> pos;
+            list.del(pos);
+            std::cout <<'\n' << "Done" <<'\n';
             break;
         case 4:
-            std::cout<<list.getSize()<<'\n';
+            std::cout << "Elemet to print: ";
+            int pos1; std::cin >> pos1;
+            std::cout << "Element: ";
+            std::cout << list.getData(pos1);
+            std::cout << '\n';
             break;
+        case 5:
+            std::cout << "List: ";
+            list.printAll();
+            std::cout << '\n';
+            break;
+        
         case 0: std::cout << "Exit\n";
             break;
         default: std::cout << "Ha-ha \n";
