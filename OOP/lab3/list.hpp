5fe8207c96c1f8c1a136d3b8575217d3525a005c
PauEnChasteaux <149692118+PauEnChasteaux@users.noreply.github.com>
2023-12-27 17:01:57 +0700

    Update list.hpp

--- OOP/lab3/list.hpp
+++ OOP/lab3/list.hpp
@@ -8,20 +8,23 @@ class List :public Container {
 		int data;
 	};
 	Node* head = nullptr;
-    int size = 0;
+	int size = 0;
 public:
 	List();
 	List(int data, Node* pNext = nullptr);
 
 	List(const List& m1);
 	List& operator=(const List& m1);
+	int& operator[](int pos)const;
+
 
 	void push(int num)override;
-    void insert(int index, int data)override;
+	void insert(int index, int data)override;
+	void megaInsert(int pos, List* list);
 	void del(int pos)override;
 
 	int getData(int pos)override;
-    int getSize()override;
+	int getSize()override;
 	void printAll()override;
 
 	~List();
